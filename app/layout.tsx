caf7504ed5ebbdf8bc15a0cc60d794a7e3b352c9
jinx6ix <irayaian15@gmail.com>
2025-08-07 15:21:40 +0300

    gtag for google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,7 +91,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Google Analytics tag */}
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5MCS8TS6');
+            `,
+          }}
+        />
+        
+        {/* Optional: Google Analytics (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q6Y2Y3PSXH"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -105,11 +118,21 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MCS8TS6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <LanguageProvider>
-          <ThemeProvider 
-            attribute="class" 
-            defaultTheme="light" 
-            enableSystem 
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
             disableTransitionOnChange
           >
             <Navbar />
@@ -118,6 +141,7 @@ export default function RootLayout({
             <AIBookingAssistant />
           </ThemeProvider>
         </LanguageProvider>
+
         <Analytics />
         <SpeedInsights />
       </body>
